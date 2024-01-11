2fdabcb2b9212666fd02b966483a74dcf19cbbf7
Alin Onea <oneaalinx@gmail.com>
2024-01-11 21:07:20 +0200

    refactor

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import threading
 def run_load():
     thread = threading.Timer(60.0, run_load)  # 60 seconds = 1 minute
     thread.start()
-    weather_dataframe = load_dataframes()
-    # weather_dataframe.show()
+    vehicles_dataframe, weather_dataframe = load_dataframes()
+    vehicles_dataframe.show(100)
+    weather_dataframe.show()
 
 run_load()
